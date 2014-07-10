75011dbe56dba44a6c98e8c270d3e64f90135fc0
Xanathar <marco@mastropaolo.com>
2014-07-10 10:03:12 +0200

    rawget/rawset

---
+++ src/MoonSharp.Interpreter/CoreLib/RawTableAccess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MoonSharp.Interpreter.Execution;
+
+namespace MoonSharp.Interpreter.CoreLib
+{
+	[MoonSharpModule]
+	public class RawTableAccess
+	{
+		[MoonSharpMethod]
+		static DynValue rawget(ScriptExecutionContext executionContext, CallbackArguments args)
+		{
+			DynValue table = args.AsType(0, "rawget", DataType.Table);
+			DynValue index = args[1];
+
+			return table.Table[index];
+		}
+
+		[MoonSharpMethod]
+		static DynValue rawset(ScriptExecutionContext executionContext, CallbackArguments args)
+		{
+			DynValue table = args.AsType(0, "rawset", DataType.Table);
+			DynValue index = args[1];
+			DynValue val = args[2];
+
+			table.Table[index] = val;
+
+			return table;
+		}
+	}
+}
--- src/MoonSharp.Interpreter/Modules/CoreModules.cs
+++ src/MoonSharp.Interpreter/Modules/CoreModules.cs
@@ -14,12 +14,13 @@ namespace MoonSharp.Interpreter
 		String = 0x8,
 		LoadMethods = 0x10,
 		Table = 0x20,
+		RawTableAccess = 0x40,
 
 
 
 		Preset_HardSandbox = GlobalConsts | TableIterators | String | Table,
 		Preset_SoftSandbox = Preset_HardSandbox | Metatables,
-		Preset_Default = Preset_SoftSandbox | LoadMethods,
+		Preset_Default = Preset_SoftSandbox | LoadMethods | RawTableAccess,
 		Preset_Complete = Preset_Default,
 
 	}
