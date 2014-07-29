6008ebb21eacebc311c3819e7c7d6c0ea760a671
John Wordsworth <john@johnwordsworth.com>
2014-07-29 18:54:34 +0100

    Adds the following string module methods: char, lower, upper, rep, reverse, sub. Updated string.byte to accept negative string positions.

--- src/MoonSharp.Interpreter/CoreLib/StringModule.cs
+++ src/MoonSharp.Interpreter/CoreLib/StringModule.cs
@@ -49,16 +49,24 @@ namespace MoonSharp.Interpreter.CoreLib
 		private static DynValue PerformByteLike(DynValue vs, DynValue vi, DynValue vj, Func<int, int> filter)
 		{
 			string s = vs.String;
-			int? i = AdjustIndex(s, vi, 0);
-			int? j = AdjustIndex(s, vj, i ?? 0);
+            int conv_i = vi.IsNil() ? 1 : (int)vi.Number;
+            int conv_j = vj.IsNil() ? conv_i : (int)vj.Number;
 
-			if (i == null || j == null || i > j)
-				return DynValue.Nil;
+            StringRange range = StringRange.FromLuaRange(conv_i, conv_j);
+            range.MapToString(vs.String);
 
-			DynValue[] rets = new DynValue[j.Value - i.Value + 1];
+            if ((range.Start >= vs.String.Length) || (range.End < range.Start))
+            {
+                return DynValue.NewString("");
+            }
 
-			for (int ii = i.Value; ii <= j.Value; ii++)
-				rets[ii] = DynValue.NewNumber(filter((int)s[ii]));
+            int length = range.Length();
+			DynValue[] rets = new DynValue[length];
+
+            for (int i = 0; i < length; ++i)
+            {
+                rets[i] = DynValue.NewNumber(filter((int)s[range.Start + i]));
+            }
 
 			return DynValue.NewTuple(rets);
 		}
@@ -135,5 +143,148 @@ namespace MoonSharp.Interpreter.CoreLib
 			return PatternMatching.Str_Find(v_s.String, v_p.String, i, plain);
 		}
 
+        [MoonSharpMethod()]
+        public static DynValue @char(ScriptExecutionContext executionContext, CallbackArguments args)
+        {
+            StringBuilder result = new StringBuilder(args.Count);
+
+            for (int i = 0; i < args.Count; ++i)
+            {
+                DynValue value = args.AsType(i, "char", DataType.Number, false);
+                result.Append((char)value.Number);
+            }
+
+            return DynValue.NewString(result.ToString());
+        }
+
+        [MoonSharpMethod()]
+        public static DynValue lower(ScriptExecutionContext executionContext, CallbackArguments args)
+        {
+            DynValue arg_s = args.AsType(0, "lower", DataType.String, false);
+
+            return DynValue.NewString(arg_s.String.ToLower());
+        }
+
+        [MoonSharpMethod()]
+        public static DynValue upper(ScriptExecutionContext executionContext, CallbackArguments args)
+        {
+            DynValue arg_s = args.AsType(0, "upper", DataType.String, false);
+
+            return DynValue.NewString(arg_s.String.ToUpper());
+        }
+
+        [MoonSharpMethod()]
+        public static DynValue rep(ScriptExecutionContext executionContext, CallbackArguments args)
+        {
+            DynValue arg_s = args.AsType(0, "rep", DataType.String, false);
+            DynValue arg_n = args.AsType(1, "rep", DataType.Number, false);
+
+            if (String.IsNullOrEmpty(arg_s.String) || (arg_n.Number < 1))
+            {
+                return DynValue.NewString("");
+            }
+
+            int count = (int)arg_n.Number;
+            StringBuilder result = new StringBuilder(arg_s.String.Length * count);
+
+            for (int i = 0; i < count; ++i)
+            {
+                result.Append(arg_s.String);
+            }
+
+            return DynValue.NewString(result.ToString());
+        }
+
+        [MoonSharpMethod()]
+        public static DynValue reverse(ScriptExecutionContext executionContext, CallbackArguments args)
+        {
+            DynValue arg_s = args.AsType(0, "reverse", DataType.String, false);
+
+            if (String.IsNullOrEmpty(arg_s.String))
+            {
+                return DynValue.NewString("");
+            }
+
+            char[] elements = arg_s.String.ToCharArray();
+            Array.Reverse(elements);
+
+            return DynValue.NewString(new String(elements));
+        }
+
+        [MoonSharpMethod()]
+        public static DynValue sub(ScriptExecutionContext executionContext, CallbackArguments args)
+        {
+            DynValue arg_s = args.AsType(0, "sub", DataType.String, false);
+            DynValue arg_i = args.AsType(1, "sub", DataType.Number, false);
+            DynValue arg_j = args.AsType(2, "sub", DataType.Number, true);
+
+            int i = arg_i.IsNil() ? 1 : (int)arg_i.Number;
+            int j = arg_j.IsNil() ? -1 : (int)arg_j.Number;
+
+            StringRange range = StringRange.FromLuaRange(i, j);
+            range.MapToString(arg_s.String);
+
+            if ((range.Start >= arg_s.String.Length) || (range.End < range.Start))
+            {
+                return DynValue.NewString("");
+            }
+
+            return DynValue.NewString(arg_s.String.Substring(range.Start, range.Length()));
+        }
 	}
+
+    public class StringRange
+    {
+        public int Start;
+        public int End;
+
+        public StringRange()
+        {
+            Start = 0;
+            End = 0;
+        }
+
+        public StringRange(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public static StringRange FromLuaRange(int start, int end)
+        {
+            StringRange range = new StringRange();
+            range.Start = (start > 0) ? start - 1 : start;
+            range.End = (end > 0) ? end - 1 : end;
+
+            return range;
+        }
+
+        public void MapToString(String value)
+        {
+            if (Start < 0)
+            {
+                Start = value.Length + Start;
+            }
+
+            if (Start < 0)
+            {
+                Start = 0;
+            }
+
+            if (End < 0)
+            {
+                End = value.Length + End;
+            }
+
+            if (End >= value.Length)
+            {
+                End = value.Length - 1;
+            }
+        }
+
+        public int Length()
+        {
+            return (End - Start) + 1;
+        }
+    }
 }
