2b208b6ed0513f43ca997f3a810da4b3747fc903
Xanathar <marco@mastropaolo.com>
2015-03-18 19:21:05 +0100

    revied platform mechanism

---
+++ src/MoonSharp.Interpreter/Loaders/IFileSystemAccessor.cs
@@ -0,0 +1,15 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+
+//namespace MoonSharp.Interpreter.Loaders
+//{
+//	/// <summary>
+//	/// An interface for wrapping all file system accesses made by MoonSharp scripts.
+//	/// </summary>
+//	public interface IFileSystemAccessor 
+//	{
+//	}
+//}
