5d30ede54810aa7a30df29f420d7723370ebf620
Xanathar <marco@mastropaolo.com>
2015-05-20 18:48:49 +0200

    Version number to 0.9.4, tested with Unity

---
+++ src/Unity/UnityTestBed/ResynchAssets.cmd
@@ -0,0 +1,5 @@
+copy ..\..\MoonSharp.Interpreter.Tests\bin\Release\*.dll Assets\Plugins
+copy ..\..\MoonSharp.Interpreter.Tests\bin\Release\*.pdb Assets\Plugins
+copy ..\..\MoonSharp.Interpreter.Tests\bin\Release\*.xml Assets\Plugins
+
+pause
