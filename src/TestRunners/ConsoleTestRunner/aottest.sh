c0fe3a789b841fbf570d22ecd82c12380d26e5d1
Xanathar <marco@mastropaolo.com>
2016-01-23 22:19:19 +0100

    wip: incremented trampolines for AOT CI tests

--- src/TestRunners/ConsoleTestRunner/aottest.sh
+++ src/TestRunners/ConsoleTestRunner/aottest.sh
@@ -1,4 +1,4 @@
 mono --debug --aot=full ConsoleTestRunner.exe
 mono --debug --aot=full *.dll
-mono --full-aot ConsoleTestRunner.exe
+mono --full-aot --aot=nimt-trampolines=4096 ConsoleTestRunner.exe
 exit $?
