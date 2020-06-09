1449e0637ce7f623d7a26415094c487de200112c
Robin Rolf <imer@imer.cc>
2020-06-09 10:03:08 +0200

    Proof of concept for enumerator clr coroutines

---
+++ src/MoonSharp.Interpreter/Interop/Attributes/MoonSharpClrCoroutineAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MoonSharp.Interpreter
+{
+    /// <summary>
+    /// Marks a method as a clr enumerator based coroutine
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = true)]
+    public sealed class MoonSharpClrCoroutineAttribute : Attribute
+	{
+        public MoonSharpClrCoroutineAttribute()
+		{
+        }
+    }
+}
