dd5316459448e6dd12ff9f82139409776b05ce80
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2021-03-27 18:22:55 +1100

    Fixed table.insert mishandling nil values inserts.
    Closes #299

--- src/Unity/MoonSharp/Assets/Plugins/MoonSharp/Interpreter/CoreLib/TableModule.cs
+++ src/Unity/MoonSharp/Assets/Plugins/MoonSharp/Interpreter/CoreLib/TableModule.cs
@@ -137,11 +137,11 @@ namespace MoonSharp.Interpreter.CoreLib
 
 			if (args.Count > 3)
 				throw new ScriptRuntimeException("wrong number of arguments to 'insert'");
-			
+
 			int len = GetTableLength(executionContext, vlist);
 			Table list = vlist.Table;
 
-			if (vvalue.IsNil())
+			if (args.Count == 2)
 			{
 				vvalue = vpos;
 				vpos = DynValue.NewNumber(len + 1);
