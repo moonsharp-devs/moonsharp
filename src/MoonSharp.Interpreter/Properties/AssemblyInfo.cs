9b01d3b2716d58047cf03f86ca345deb3d24673a
Ben Askren <ben@buildcalc.com>
2022-12-02 18:57:20 -0500

    FIX (net6.0-ios, net6.0-wasm) : https://github.com/dotnet/linker/issues/3072

--- src/MoonSharp.Interpreter/Properties/AssemblyInfo.cs
+++ src/MoonSharp.Interpreter/Properties/AssemblyInfo.cs
@@ -5,6 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
+#if !NET6_0
+
 [assembly: AssemblyTitle("MoonSharp.Interpreter")]
 [assembly: AssemblyDescription("An interpreter for the Lua language")]
 [assembly: AssemblyConfiguration("")]
@@ -34,6 +37,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion(MoonSharp.Interpreter.Script.VERSION)]
 [assembly: AssemblyFileVersion(MoonSharp.Interpreter.Script.VERSION)]
+#endif
 
 // Give 
 [assembly: InternalsVisibleTo("MoonSharp.Interpreter.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100F704C50BBDC3F2F011CC26A8C6C4797A40E0B4BC94CFB1335E9BA208326340696B686DC13099F10D3054544532F5E3E66C26A13FF260AEA2343E0410511FE56EDCC2AFB898AAA1BC21DA33C0D0AE60824EB441D02A0E6B7AE251CDE0946BFC748209C12B062573ECDB008A3D10CC40534B314847591CE5342A3BC6AA83CE23B8")]
